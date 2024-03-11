2e2b68ed9693bf2d804b2f0f86b612faa249069e
joemellis <joemellis1983@gmail.com>
2024-03-11 21:38:55 +0000

    data adding to cards and displaying

--- home/views.py
+++ home/views.py
@@ -3,15 +3,6 @@ from .models import Home
 
 #def index(request):
 #    return render(request,'home/index.html')
-    
 def index(request):
-    home_list = Home.objects.all()  # Replace with your actual queryset
+    home_list = Home.objects.all()
     return render(request, 'home/index.html', {'home_list': home_list})
-
-def index(request):
-    dummy_data = [
-        {'title': 'Dummy Item 1', 'price': 100.0, 'image': '/path/to/image1.jpg'},
-        {'title': 'Dummy Item 2', 'price': 200.0, 'image': '/path/to/image2.jpg'},
-        # Add more dummy data as needed
-    ]
-    return render(request, 'home/index.html', {'home_list': dummy_data})

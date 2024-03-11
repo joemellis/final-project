3c5d993275d478003cfa9774333851273d0b2006
joemellis <joemellis1983@gmail.com>
2024-03-11 16:55:47 +0000

    requirements

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+
 # Create your models here.
 
 
@@ -7,4 +8,9 @@ class Home(models.Model):
     title = models.CharField(max_length = 100)
     image = CloudinaryField('image',default = 'placeholder')
     info = models.TextField(max_length = 500)
-    price =  models.CharField(max_length = 100)
+    location = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title

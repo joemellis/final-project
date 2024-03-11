8a2956c649a887e7c66157ba77efb7095626895b
joemellis <joemellis1983@gmail.com>
2024-03-11 14:32:34 +0000

    added Home Model,added elephantsql

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
+
+
+class Home(models.Model):
+    title = models.CharField(max_length = 100)
+    image = CloudinaryField('image',default = 'placeholder')
+    info = models.TextField(max_length = 500)
+    price =  models.CharField(max_length = 100)
--- rustys_trading_post/settings.py
+++ rustys_trading_post/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os 
+import dj_database_url
+
 
 if os.path.isfile('env.py'):
     import env
@@ -89,12 +91,20 @@ WSGI_APPLICATION = 'rustys_trading_post.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+if "DEBUG" in os.environ:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+else: 
+    DATABASES = {
+        default:{dj_database_url.parse(os.environ.get('DB_URL'))
+    }
+}
+
 
 
 # Password validation
